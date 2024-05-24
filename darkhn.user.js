d3b7873537a4fc1ee8c27a5ed430301e9fd9f0d9
Susam Pal <susam@susam.net>
2024-05-24 10:43:21 +0100

    Set saturation to zero in DarkHN

--- darkhn.user.js
+++ darkhn.user.js
@@ -3,6 +3,6 @@
 // @match https://news.ycombinator.com/*
 // ==/UserScript==
 window.document.body.style.background = '#111'
-window.document.body.style.filter = 'invert(1) hue-rotate(180deg)'
+window.document.body.style.filter = 'saturate(0) invert(1) hue-rotate(180deg)'
 window.document.getElementsByTagName('td')[0].style.background = '#ccc'
 window.document.getElementsByTagName('img')[0].style.filter = 'saturate(0)'
